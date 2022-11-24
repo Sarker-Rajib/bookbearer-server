3e35a9a6ca17dc6004ad48713b2060a35750e7d8
Rajib Sarker <redhat0593@gmail.com>
2022-11-24 17:36:04 +0600

    server initialized

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+const app = express();
+
+
+// middleware
+app.use(cors());
+app.use(express())
+
+app.get('/', (req, res) => {
+    res.send('Welcome to the BookBearer Server!')
+})
+
+app.listen(port, () => {
+    console.log(`BookBearer server running on port ${port}`);
+});
