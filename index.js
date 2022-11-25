1ee029ddf450b5da3d89a32939bf152901b1ca73
Rajib Sarker <redhat0593@gmail.com>
2022-11-25 18:40:35 +0600

    books api created

--- index.js
+++ index.js
@@ -14,14 +14,28 @@ app.use(cors());
 const uri = `mongodb+srv://${process.env.REACT_APP_ADMIN_USER}:${process.env.REACT_APP_ADMIN_PASSWORD}@cluster0.yw8lqr5.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-console.log(uri);
 const run = async () => {
     try {
+        const booksCategoryCollection = client.db("bookStore").collection("bookCategories");
         const booksCollection = client.db("bookStore").collection("books");
 
-        app.get('/books', async (req, res) => {
+        app.get('/bookCategories', async (req, res) => {
             const query = {};
-            
+
+            const result = await booksCategoryCollection.find(query).toArray();
+            res.send(result)
+        });
+
+        app.get('/books', async (req, res) => {
+            const categoryId = parseFloat(req.query.categoryId);
+            let query = {};
+
+            if (categoryId) {
+                query = {
+                    categoryId: categoryId
+                };        
+            }
+
             const result = await booksCollection.find(query).toArray();
             res.send(result)
         });
